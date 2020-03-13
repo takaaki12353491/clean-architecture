c79cf61c954537c3e1bd6738e84c5f10d092af45
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-14 03:05:37 +0900

    Add log settings

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"cln-arch/infra/router"
+
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
-	
+	// Log settings
+	log.SetFormatter(&log.JSONFormatter{
+		PrettyPrint: true,
+	})
+	log.SetReportCaller(true)
+
+	router.Start()
 }
