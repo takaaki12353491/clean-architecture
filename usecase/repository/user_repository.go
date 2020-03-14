4f6b30f96789d77344620c06d7843d242efcee12
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-14 20:45:34 +0900

    Add functions to UserRepository

--- usecase/repository/user_repository.go
+++ usecase/repository/user_repository.go
@@ -1,10 +1,14 @@
 package repository
 
 import (
+	"cln-arch/domain/model"
 	"cln-arch/errs"
 	inputdata "cln-arch/usecase/input/data"
 )
 
 type UserRepository interface {
+	FindByID(id string) (model.User, errs.HTTPError)
 	Create(inputdata.User) errs.HTTPError
+	Update(inputdata.User) errs.HTTPError
+	Delete(id string) errs.HTTPError
 }
