59d2f1a947d02d19afc2b09372c58dfefc178558
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-19 02:27:59 +0900

    Modify logformat

--- infra/server/server.go
+++ infra/server/server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"os"
+	"strings"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
@@ -10,10 +13,8 @@ import (
 func Start() {
 	// Echo instance
 	e := echo.New()
-
 	// Middleware
 	e.Use(
-		middleware.Logger(),
 		middleware.Recover(),
 		func(h echo.HandlerFunc) echo.HandlerFunc {
 			return func(c echo.Context) error {
@@ -22,6 +23,10 @@ func Start() {
 				})
 			}
 		},
+		middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: logFormat(),
+			Output: os.Stdout,
+		}),
 	)
 
 	// Handlers
@@ -35,4 +40,31 @@ func Start() {
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func logFormat() string {
+	// Refer to https://github.com/tkuchiki/alp
+	var format string
+	strings.Join([]string{
+		"time:${time_rfc3339}\t\n",
+		"host:${remote_ip}\t\n",
+		"forwardedfor:${header:x-forwarded-for}\t\n",
+		"req:-\t\n",
+		"status:${status}\t\n",
+		"method:${method}\t\n",
+		"uri:${uri}\t\n",
+		"size:${bytes_out}\t\n",
+		"referer:${referer}\t\n",
+		"ua:${user_agent}\t\n",
+		"reqtime_ns:${latency}\t\n",
+		"cache:-\t\n",
+		"runtime:-\t\n",
+		"apptime:-\t\n",
+		"vhost:${host}\t\n",
+		"reqtime_human:${latency_human}\t\n",
+		"x-request-id:${id}\t\n",
+		"host:${host}\n",
+	}, "")
+
+	return format
 }
