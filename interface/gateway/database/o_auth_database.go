bc1c9c15113198e95bdbcf6749113a85372ef4e1
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-19 02:26:12 +0900

    Modify database

--- interface/gateway/database/o_auth_database.go
+++ interface/gateway/database/o_auth_database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"cln-arch/domain/model"
 	"cln-arch/usecase/repository"
-	"time"
 )
 
 type OAuthDatabase struct {
@@ -14,21 +13,21 @@ func NewOAuthDatabase() repository.OAuthRepository {
 	return &OAuthDatabase{}
 }
 
-func (db *OAuthDatabase) FindStateBySession(session *model.Session) (string, error) {
-	return "", nil
+func (db *OAuthDatabase) FindUserStateBySessionID(string) (*model.UserState, error) {
+	return nil, nil
 }
-func (db *OAuthDatabase) FindBySessionIDAndUserToken(sessionID string, token string) (*time.Time, int, error) {
-	return nil, 0, nil
+func (db *OAuthDatabase) FindUserStateBySessionIDAndUserToken(sessionID string, token string) (*model.UserState, error) {
+	return nil, nil
 }
-func (db *OAuthDatabase) FindByUserTokenID(id int) (*model.GithubToken, error) {
-	return &model.GithubToken{}, nil
+func (db *OAuthDatabase) FindOAuthTokenByUserID(string) (*model.OAuthToken, error) {
+	return nil, nil
 }
-func (db *OAuthDatabase) StoreState(state string, session *model.Session, expiry *time.Time) error {
+func (db *OAuthDatabase) StoreUserState(*model.UserState) error {
 	return nil
 }
-func (db *OAuthDatabase) StoreUserToken(model *model.Session, userToken *model.UserToken, id int) (int, error) {
-	return 0, nil
+func (db *OAuthDatabase) StoreOAuthToken(*model.OAuthToken) error {
+	return nil
 }
-func (db *OAuthDatabase) StoreGithubToken(githubToken *model.GithubToken) (int, error) {
-	return 0, nil
+func (db *OAuthDatabase) StoreUserToken(*model.UserToken) error {
+	return nil
 }
