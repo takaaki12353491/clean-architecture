35f80260ffd9b058ec45653fc28c7ebf5586b95c
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-19 02:22:24 +0900

    Modify OAuth input port

--- usecase/input/port/o_auth_input_port.go
+++ usecase/input/port/o_auth_input_port.go
@@ -3,11 +3,10 @@ package inputport
 import (
 	inputdata "cln-arch/usecase/input/data"
 	outputdata "cln-arch/usecase/output/data"
-	"time"
 )
 
 type OAuthInputPort interface {
-	Login(login *inputdata.Login, _session *inputdata.Session, expiry *time.Time) (*outputdata.Login, error)
-	Callback(callback *inputdata.Callback, _githubToken *inputdata.GithubToken, _userToken *inputdata.UserToken) (*outputdata.Callback, error)
+	Login(*inputdata.Login) (*outputdata.Login, error)
+	Callback(*inputdata.Callback) (*outputdata.Callback, error)
 	Auth(auth *inputdata.Auth) (*outputdata.Auth, error)
 }
