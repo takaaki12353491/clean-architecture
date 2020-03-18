0c99f205e73dc2582ad65563b7d9f5b0ee6c1219
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-19 02:27:11 +0900

    Modify sql

--- mysql/user_sql.sql
+++ mysql/user_sql.sql
@@ -13,20 +13,28 @@ CREATE TABLE `user_states` (
     `user_id` VARCHAR(36) PRIMARY KEY,
     `session_id` VARCHAR(255) NOT NULL,
     `state` VARCHAR(255),
-    `token` VARCHAR(255),
     `expiry` TIMESTAMP NOT NULL,
-    `github_tokens_id` VARCHAR(36) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(`user_id`) REFERENCES `user`(`id`),
     FOREIGN KEY(`github_token_id`) REFERENCES `github_tokens`(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ROW_FORMAT = DYNAMIC;
 
+DROP TABLE IF EXISTS `user_tokens`;
+CREATE TABLE `user_token` (
+    `user_id` VARCHAR(36) PRIMARY KEY,
+    `token` VARCHAR(255),
+    `expiry` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(`user_id`) REFERENCES `user`(`id`),
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ROW_FORMAT = DYNAMIC;
+
 DROP TABLE IF EXISTS `github_tokens`;
 CREATE TABLE `github_tokens` (
-    `id` VARCHAR(36) PRIMARY KEY,
+    `user_id` VARCHAR(36) PRIMARY KEY,
     `token` VARCHAR(255) NOT NULL,
     `type` VARCHAR(255) NOT NULL,
     `refresh_token` VARCHAR(255) NOT NULL,
     `expiry` TIMESTAMP NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(`user_id`) REFERENCES `user`(`id`),
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ROW_FORMAT = DYNAMIC;
