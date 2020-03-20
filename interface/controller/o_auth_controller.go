96536530562127bd54e4f0071709deb93fe8d944
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-20 18:38:57 +0900

    Modify interface

--- interface/controller/o_auth_controller.go
+++ interface/controller/o_auth_controller.go
@@ -2,16 +2,14 @@ package controller
 
 import (
 	"cln-arch/config"
-	"cln-arch/interface/gateway/database"
+	"cln-arch/infra/database"
 	"cln-arch/interface/presenter"
 	inputdata "cln-arch/usecase/input/data"
 	inputport "cln-arch/usecase/input/port"
 	"cln-arch/usecase/interactor"
 	outputdata "cln-arch/usecase/output/data"
 	"context"
-	"time"
 
-	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -28,40 +26,20 @@ func NewOAuthController() *OAuthController {
 	}
 }
 
-func (ct *OAuthController) Login(iSession *inputdata.Session) (*outputdata.Login, error) {
-	userID := uuid.New().String()
-	state := createRand()
-	serverConf := config.NewServer()
-	url := serverConf.Github.AuthCodeURL(state)
-	expiry := time.Now().Add(10 * time.Minute)
-	iLogin := &inputdata.Login{
-		UserID:  userID,
-		State:   state,
-		URL:     url,
-		Session: iSession,
-		Expiry:  &expiry,
-	}
-	return ct.inputport.Login(iLogin)
+func (ct *OAuthController) Auth() (*outputdata.Auth, error) {
+	return ct.inputport.Auth()
 }
 
 func (ct *OAuthController) Callback(iCallbackRequest *inputdata.CallbackRequest) (*outputdata.Callback, error) {
-	serverConf := config.NewServer()
-	oauthToken, err := serverConf.Github.Exchange(context.Background(), iCallbackRequest.Code)
+	githubConf := config.NewGithubConf()
+	oauthToken, err := githubConf.Exchange(context.Background(), iCallbackRequest.Code)
 	if err != nil {
 		log.Error(err)
 		return nil, err
 	}
-	token := createRand()
-	expiry := time.Now().Add(7 * 24 * time.Hour)
 	iCallback := &inputdata.Callback{
 		Request:    iCallbackRequest,
 		OAuthToken: oauthToken,
-		Token:      token,
-		Expiry:     &expiry,
 	}
 	return ct.inputport.Callback(iCallback)
-}
-
-func (ct *OAuthController) Auth(iAuth *inputdata.Auth) (*outputdata.Auth, error) {
-	return ct.inputport.Auth(iAuth)
 }
--- interface/presenter/o_auth_presenter.go
+++ interface/presenter/o_auth_presenter.go
@@ -1,6 +1,7 @@
 package presenter
 
 import (
+	"cln-arch/config"
 	"cln-arch/domain/model"
 	outputdata "cln-arch/usecase/output/data"
 	outputport "cln-arch/usecase/output/port"
@@ -13,21 +14,17 @@ func NewOAuthPresenter() outputport.OAuthOutputPort {
 	return &OAuthPresenter{}
 }
 
-func (pre *OAuthPresenter) Login(login *model.Login) *outputdata.Login {
-	return &outputdata.Login{
-		State: login.State,
-		URL:   login.URL,
-	}
-}
-
-func (pre *OAuthPresenter) Callback(userToken *model.UserToken) *outputdata.Callback {
-	return &outputdata.Callback{
-		Token: userToken.Token,
-	}
-}
-
-func (pre *OAuthPresenter) Auth(oauthToken *model.OAuthToken) *outputdata.Auth {
+func (pre *OAuthPresenter) Auth(state *model.OAuthState) *outputdata.Auth {
+	oauthConfig := config.NewGithubConf()
+	url := oauthConfig.AuthCodeURL(state.State)
 	return &outputdata.Auth{
-		OAuthToken: oauthToken.Token,
+		State: state.State,
+		URL:   url,
+	}
+}
+
+func (pre *OAuthPresenter) Callback(token *model.OAuthToken) *outputdata.Callback {
+	return &outputdata.Callback{
+		Token: token.Token.AccessToken,
 	}
 }
