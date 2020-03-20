e8e3510a2fc23a3a6a973af501aca0212e361d2a
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-20 19:27:26 +0900

    Add gorm to domain model

--- domain/model/o_auth.go
+++ domain/model/o_auth.go
@@ -4,13 +4,15 @@ import (
 	"cln-arch/validator"
 	"time"
 
+	"github.com/jinzhu/gorm"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 )
 
 type OAuthState struct {
-	State  string
-	Expiry *time.Time
+	gorm.Model
+	State  string     `gorm:"column:state"`
+	Expiry *time.Time `gorm:"column:expiry"`
 }
 
 func NewOAuthState() (*OAuthState, error) {
@@ -30,14 +32,21 @@ func NewOAuthState() (*OAuthState, error) {
 
 // OAuthToken is oauth token
 type OAuthToken struct {
-	User  *User
-	Token *oauth2.Token
+	gorm.Model
+	User         *User
+	AccessToken  string     `gorm:"access_token"`
+	TokenType    string     `gorm:"token_type"`
+	RefreshToken string     `gorm:"refresh_token"`
+	Expiry       *time.Time `gorm:"expiry"`
 }
 
 func NewOAuthToken(user *User, token *oauth2.Token) (*OAuthToken, error) {
 	oauthToken := &OAuthToken{
-		User:  user,
-		Token: token,
+		User:         user,
+		AccessToken:  token.AccessToken,
+		TokenType:    token.TokenType,
+		RefreshToken: token.RefreshToken,
+		Expiry:       &token.Expiry,
 	}
 	err := validator.Validate(oauthToken)
 	if err != nil {
