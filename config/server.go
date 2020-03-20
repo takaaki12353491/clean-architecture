46236cb02102dce0dfb3f0cb737b5fc0eed2fb89
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-20 18:36:58 +0900

    Delete newServerConf and create newGithubConf

--- config/server.go
+++ config/server.go
@@ -7,33 +7,7 @@ import (
 	oauth2github "golang.org/x/oauth2/github"
 )
 
-// ServerConf is above all
-type ServerConf struct {
-	DBConf *DBConf
-	Github *oauth2.Config
-}
-
-// DBConf is config using DB
-type DBConf struct {
-	Database string
-	DSN      string
-}
-
-func NewServer() *ServerConf {
-	return &ServerConf{
-		DBConf: getDBConf(),
-		Github: getGithubConf(),
-	}
-}
-
-func getDBConf() *DBConf {
-	return &DBConf{
-		Database: os.Getenv("DATABASE"),
-		DSN:      os.Getenv("DSN"),
-	}
-}
-
-func getGithubConf() *oauth2.Config {
+func NewGithubConf() *oauth2.Config {
 	scopes := []string{"repo"}
 	return &oauth2.Config{
 		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
