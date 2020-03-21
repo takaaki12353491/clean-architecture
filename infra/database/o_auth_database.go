edce4e55fd97f57c925f00b7f2e9e80c124799ba
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-21 16:25:11 +0900

    Add error handling to oauth database

--- infra/database/o_auth_database.go
+++ infra/database/o_auth_database.go
@@ -3,6 +3,8 @@ package database
 import (
 	"cln-arch/domain/model"
 	"cln-arch/usecase/repository"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type OAuthDatabase struct {
@@ -15,22 +17,34 @@ func NewOAuthDatabase() repository.OAuthRepository {
 
 func (db *OAuthDatabase) FindStateByState(state string) (*model.OAuthState, error) {
 	oauthState := &model.OAuthState{}
-	db.First(oauthState, "state = ?", state)
+	if err := db.First(oauthState, "state = ?", state).Error; err != nil {
+		log.WithFields(log.Fields{}).Error(err)
+		return nil, err
+	}
 	return oauthState, nil
 }
 
 func (db *OAuthDatabase) FindTokenByToken(token string) (*model.OAuthToken, error) {
 	oauthToken := &model.OAuthToken{}
-	db.First(oauthToken, "access_token = ?", token)
+	if err := db.First(oauthToken, "access_token = ?", token).Error; err != nil {
+		log.WithFields(log.Fields{}).Error(err)
+		return nil, err
+	}
 	return oauthToken, nil
 }
 
 func (db *OAuthDatabase) StoreState(state *model.OAuthState) error {
-	db.Create(state)
+	if err := db.Create(state).Error; err != nil {
+		log.WithFields(log.Fields{}).Error(err)
+		return err
+	}
 	return nil
 }
 
 func (db *OAuthDatabase) StoreToken(token *model.OAuthToken) error {
-	db.Create(token)
+	if err := db.Create(token).Error; err != nil {
+		log.WithFields(log.Fields{}).Error(err)
+		return err
+	}
 	return nil
 }
--- infra/database/sql_handler.go
+++ infra/database/sql_handler.go
@@ -15,9 +15,10 @@ func NewSQLHandler() *SQLHandler {
 	DBMS := "mysql"
 	USER := os.Getenv("DB_USER")
 	PASS := os.Getenv("DB_PASS")
-	PROTOCOL := "tcp"
+	PROTOCOL := "tcp(127.0.0.1:3306)"
 	DBNAME := "cln_arch"
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	OPTION := "?parseTime=true&loc=Asia%2FTokyo"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + OPTION
 	db, err := gorm.Open(DBMS, CONNECT)
 	if err != nil {
 		log.Panicln(err)
