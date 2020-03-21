6332be73bc542d78e5bbcc514ea978e4e3ea281d
takaaki12353491 <takaaki12353491@gmail.com>
2020-03-22 07:49:54 +0900

    Modify GithubConf

--- config/server.go
+++ config/server.go
@@ -4,16 +4,16 @@ import (
 	"os"
 
 	"golang.org/x/oauth2"
-	oauth2github "golang.org/x/oauth2/github"
+	"golang.org/x/oauth2/github"
 )
 
 func NewGithubConf() *oauth2.Config {
-	scopes := []string{"repo"}
+	scopes := []string{"read:user"}
 	return &oauth2.Config{
 		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
 		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
 		RedirectURL:  "http://localhost:8080/auth/github/callback",
 		Scopes:       scopes,
-		Endpoint:     oauth2github.Endpoint,
+		Endpoint:     github.Endpoint,
 	}
 }
